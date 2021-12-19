90a78d352f6f271cc4e82eb964b7387edcea1da9
rishabh <rishabhv47@gmail.com>
2021-12-19 17:56:31 +0530

    route

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 // initialising variables
-const express = require("express");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const path = require("path");
-const dotenv = require("dotenv");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const corsOptions = {
   origin: (origin, callback) => {
     callback(null, true);
@@ -15,9 +15,9 @@ const corsOptions = {
 dotenv.config();
 const app = express();
 
-const placeRoute = require("./place_route/place");
-const customerRoute = require("./auth_routes/customer_auth");
-const uploadRoute = require("./upload/single_upload");
+const placeRoute = require('./place_route/place');
+const customerRoute = require('./auth_routes/customer_auth');
+const uploadRoute = require('./upload/single_upload');
 
 //applying middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,16 +25,19 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 
-app.use("/v1", customerRoute);
-app.use("/place", placeRoute);
-app.use("/upload", uploadRoute);
+app.use('/v1', customerRoute);
+app.use('/place', placeRoute);
+app.use('/upload', uploadRoute);
 
+app.get('/', (req, res) => {
+  res.send({ message: 'working' });
+});
 const PORT = process.env.PORT;
 
 app.listen(PORT || 8080, () => {

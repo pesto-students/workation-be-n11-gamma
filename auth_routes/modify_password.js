f926fb92335cf56225aee4783c69d41387c813d8
rishabh <rishabhv47@gmail.com>
2021-12-19 16:42:50 +0530

    [rishabh] [workation] [checkdeploy]

---
+++ auth_routes/modify_password.js
@@ -0,0 +1,14 @@
+const bcrypt = require("bcrypt");
+const modifyPassword = (req, res, next) => {
+  const saltRound = parseInt(process.env.MY_SALT);
+  bcrypt.hash(req.body.userPassword, saltRound, (err, hashedPassword) => {
+    if (err) {
+      next(err);
+    } else {
+      req.body.password = hashedPassword;
+      next();
+    }
+  });
+};
+
+module.exports = modifyPassword;
---
+++ database/config.js
@@ -0,0 +1,24 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../service_firebase.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+let db = admin.firestore();
+
+let Users = db.collection("Users");
+let cities = db.collection("cities");
+let places = db.collection("places");
+let hotelOwners = db.collection("owners");
+let landingvideo = db.collection("landingVideos");
+
+const dBase = {
+  Users,
+  cities,
+  places,
+  hotelOwners,
+  landingvideo,
+};
+module.exports = dBase;
